eb61752c804d37e34d69038fdbde10d4cc793dc6
Anand3891 <anand.imac.sri@outlook.com>
2021-08-20 17:09:16 -0700

    ok

---
+++ script.js
@@ -0,0 +1,30 @@
+window.onload = () => {
+  document.getElementById("copy-btn").setAttribute("disabled", "disabled");
+};
+
+//hello fellow human
+
+function goButtonClicked() {
+  const textValue = document.getElementById("textarea").value;
+
+  function binaryAgent(str) {
+    return str
+      .split(" ")
+      .map((x) => String.fromCharCode(parseInt(x, 2)))
+      .join("");
+  }
+  let newString = binaryAgent(textValue);
+
+  document.getElementById("resultingText").innerHTML = newString;
+  document.getElementById("copy-btn").removeAttribute("disabled");
+}
+
+function copyText() {
+  var text = document.getElementById("resultingText").innerText;
+  var elem = document.createElement("textarea");
+  document.body.appendChild(elem);
+  elem.value = text;
+  elem.select();
+  document.execCommand("copy");
+  document.body.removeChild(elem);
+}
