61101584bf768c33d03fe56efff866ea39241d73
Anand3891 <anand.imac.sri@outlook.com>
2021-08-23 15:59:26 -0700

    adding open file and saving dynamic binary ile + bug fixes

--- script.js
+++ script.js
@@ -2,29 +2,33 @@ window.onload = () => {
   document.getElementById("copy-btn").setAttribute("disabled", "disabled");
 };
 
-function goButtonClicked() {
+function convertToBinary() {
   const textValue = document.getElementById("textarea").value;
 
-  function binaryAgent(str) {
+  function binaryConverter(str) {
     return str
       .split(" ")
       .map((x) => String.fromCharCode(parseInt(x, 2)))
       .join("");
   }
-  let newString = binaryAgent(textValue);
+  let newString = binaryConverter(textValue);
 
   document.getElementById("textareayuh").innerHTML = newString;
   document.getElementById("copy-btn").removeAttribute("disabled");
+  document
+    .getElementById("notacopybtnbutimoitsjustasgoodrightsfornoncopybuttons")
+    .removeAttribute("disabled");
 }
 
 function copyText() {
+  var copyText = document.getElementById("textareayuh");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(copyText.value);
   document.getElementById("copy-btn").innerHTML = "Copied!";
   document.getElementById("copy-btn").className = "btn btn-success";
-  var text = document.getElementById("textareayuh").innerText;
-  var elem = document.createElement("textarea");
-  document.body.appendChild(elem);
-  elem.value = text;
-  elem.select();
-  document.execCommand("copy");
-  document.body.removeChild(elem);
+  setTimeout(function () {
+    document.getElementById("copy-btn").innerHTML = "Copy Text";
+    document.getElementById("copy-btn").className = "btn btn-secondary";
+  }, 5000);
 }
